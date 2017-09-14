a659d2fcaaeed3b351d8330f601b70c44620fcaa
naveenbakde <bakde.naveen@gmail.com>
2017-09-14 09:23:38 +0530

    Remove unwanted class

--- spring-webapp/src/main/java/com/mybank/main/Application.java
+++ spring-webapp/src/main/java/com/mybank/main/Application.java
@@ -1,11 +1,11 @@
 package com.mybank.main;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		
+		SpringApplication.run(Appendable.class, args);
 	}
-
 }
--- spring-webapp/src/main/java/com/naveen/test/FirstJavaClass.java
+++
@@ -1,9 +0,0 @@
-package com.naveen.test;
-
-public class FirstJavaClass {
-
-	public static void main(String[] args) {
-		System.out.println("Hello...");
-	}
-
-}
