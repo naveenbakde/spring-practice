7067ffbb77f8941be522b9a453a68aab9932bbb2
naveenbakde <bakde.naveen@gmail.com>
2017-09-16 14:57:19 +0530

    Added H2

--- spring-webapp/src/main/java/com/spring/mybank/Application.java
+++ spring-webapp/src/main/java/com/spring/mybank/Application.java
@@ -1,11 +1,17 @@
-package com.mybank.main;
+package com.spring.mybank;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Application {
 	public static void main(String[] args) {
+		log.info("APPLICATION STARTED RUNNING");
 		SpringApplication.run(Application.class, args);
 	}
 }
